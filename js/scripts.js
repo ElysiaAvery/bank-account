e0f608881f62fc81119d1abb2369c3517dd2d538
elysia nason <elysia.avery@gmail.com>
2016-08-23 20:57:27 -0700

    add working bank account

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,61 @@
 // Business logic
 
-var person = [];
-function Account(name, balance, deposit, withdrawal) {
-  this.name = name;
-  this.balance = balance;
-  this.deposit = deposit;
-  this.withdrawal = withdrawal;
+function BankAccount(name, deposit) {
+  this.accountName = name;
+  this.accountInitialDeposit = deposit;
+  this.accountDeposit = [];
+  this.accountWithdraw = [];
+};
+
+BankAccount.prototype.accountAdd = function() {
+  return this.accountInitialDeposit += this.accountDeposit[0];
 }
 
-Account.prototype.total = function() {
-  this.balance = this.balance - this.withdrawal + this.deposit;
-  return this.balance;
+BankAccount.prototype.accountSubtract = function() {
+  return this.accountInitialDeposit -= this.accountWithdraw[0];
 }
 
+var resetFields = function() {
+  $("input#name").val("")
+  $("input#initial-deposit").val("")
+  $("input#deposit").val("");
+  $("input#withdrawal").val("");
+}
 
 //UI Logic
 $(document).ready(function() {
-  $("form").submit(function(event){
+  $("form#initiate").submit(function(event){
     event.preventDefault();
     var inputtedName = $("input#name").val();
     var inputtedInitialDeposit = parseInt($("input#initial-deposit").val());
+
+    var newAccount = new BankAccount(inputtedName, inputtedInitialDeposit);
+
+    $("#account").append("<li><span class='account-list'>See " + newAccount.accountName + "'s balance</span></li>");
+    $(".account-list").last().click(function() {
+    $("#show-account").show();
+    $("#show-account h2").text(newAccount.accountName);
+    $("#display-name").text(newAccount.accountName);
+    $("#enter-balance").text(newAccount.accountInitialDeposit);
+    $("#doing-work").unbind();
+  $("#doing-work").submit(function(event){
+    event.preventDefault();
     var inputtedDeposit = parseInt($("input#deposit").val());
     var inputtedWithdrawal = parseInt($("input#withdrawal").val());
-
-    var newAccount = new Account(inputtedName, inputtedInitialDeposit, inputtedDeposit, inputtedWithdrawal);
-    person.push(newAccount);
-
-
-    $(".total-balance").append("<li><span class='account'>See " + newAccount.name + "'s balance</span></li>");
-
-    $(".account").last().click(function() {
-      $("#show-account").show();
-      $("#show-account h2").text(newAccount.name);
-      $(".balance").text(newAccount.total());
-
+    if (inputtedDeposit){
+      newAccount.accountDeposit.unshift(inputtedDeposit);
+      newAccount.accountAdd();
+    }
+    if (inputtedWithdrawal){
+      newAccount.accountWithdraw.unshift(inputtedWithdrawal);
+      newAccount.accountSubtract();
+    }
+    console.log(newAccount.accountDeposit);
+    console.log(newAccount);
+    $("#enter-balance").text(newAccount.accountInitialDeposit);
+    resetFields();
     })
+  })
+  resetFields();
   });
 });
